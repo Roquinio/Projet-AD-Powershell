91be1fa6f81ce01f9577bc108769d33cbea4cf5b
Yahbs <57636356+Yahbs@users.noreply.github.com>
2020-12-26 14:42:05 +0100

    Format table settle

--- main.ps1
+++ main.ps1
@@ -136,14 +136,14 @@ function Get-Suppr {
 function Get-userModif {
 
     [str]$userList = ""
-    Get-ADUser -Filter (Read-Host -Prompt "Quel utilisateur souhaitez vous modifier ?") -SearchBase "CN=Users,DC=Force2,DC=net" | Format-Table Name  > $userList
+    Get-ADUser -Filter (Read-Host -Prompt "Quel utilisateur souhaitez vous modifier ?") -SearchBase "CN=Users,DC=Force2,DC=net" | Format-Table Name,Groupe,OU  > $userList
     [int]$modifArg = (Read-Host -Prompt "Que souhaitez-vous faire ? ?")
 
     Write-Host "1. Modifier une valeur.
     2. Suprimer une valeur."
 
     switch ($modifArg) {
-        1 { foreach ($user in $userList) { Set-ADUser -Identity $user  }  }
+        1 { foreach ($user in $userList) { Set-ADUser -Identity $user -Replace }  }
 
         2 {  }
         Default { Write-Host "Erreur, veuillez réessayer"; Get-userModif}
