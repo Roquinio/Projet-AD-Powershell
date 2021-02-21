9fd2827ec7677abe1d57d4cbedf4083a52540231
Yahbs <57636356+Yahbs@users.noreply.github.com>
2021-02-21 15:42:04 +0100

    test

--- main.ps1
+++ main.ps1
@@ -161,7 +161,7 @@ function Main-AD {
     function show-modifValeur {
 
     [str]$userList = ""
-    Get-ADUser -Filter (Read-Host -Prompt "Quel utilisateur souhaitez-vous modifier ?") -SearchBase "CN=Users,DC=Force2,DC=net" | Format-Table Name,Groupe,OU  > $userList
+    Get-ADUser -Filter (Read-Host -Prompt "Quel utilisateur souhaitez-vous modifier ?") -SearchBase "CN=Users,DC=Force2,DC=net"   > $userList
     Write-Host "==================Modifications==================
     1. Changement de groupe.
     2. Changer le mot de passe.
